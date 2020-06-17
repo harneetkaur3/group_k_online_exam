5c28e3af3282ebfe0176e38665b8806b59873515
unknown <esraa.abujayyab@gmail.com>
2020-06-17 02:50:27 +0100

    two functions added: mark_questions_and_ display_results & show_exam_results

--- src/user_maintainance.c
+++ src/user_maintainance.c
@@ -1,7 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
+
 #define range 10
+#define NUMBER_QUESTION 30
+#define STUDENT_ID "Student_ID: "
+#define GRADE "Mark: "
+
+
+struct QUESTION{
+	int q_id;
+	char question[length];
+	char option1[length];
+	char option2[length];
+	char option3[length];
+	char option4[length];
+	char ans[2];
+};
+
+struct QUESTION* exam = malloc(sizeof(QUESTION)*30);
+
+
+struct STUDENT_SOLUTION{
+	int s_id;
+	char ans[2];
+	int student_id;
+}
+
+struct STUDENT_SOLUTION* sol = malloc(sizeof(STUDENT_SOLUTION)*30);
+
+
+
 char* login(char *u_id, char *pwd){
 	FILE *user_credentials;
 	char un[range];
@@ -35,3 +65,72 @@ char* login(char *u_id, char *pwd){
 		}
 	}
 }
+
+
+int show_exam_result(){
+
+	char s;
+	
+	FILE * fPtr;
+   	
+    fPtr = fopen("STUDENT_RESULTS.txt","r"); 
+    
+    if(fPtr == NULL)
+    {
+    	printf("Unable to read results. \n");
+    	exit(EXIT_FAILURE);
+    } 	
+	
+	while((s=fgetc(fPtr))!= EOF) {
+      printf("%c",s);
+	}
+	
+	
+	fclose(fPtr);
+	
+	
+	return 0;
+}
+
+
+
+void mark_questions_and_display_results(){
+	
+	FILE * fPtr;
+   	
+    fPtr = fopen("STUDENT_RESULTS.txt","a"); 
+    
+    if(fPtr == NULL)
+    {
+    	printf("unable to create file. \n");
+    	exit(EXIT_FAILURE);
+    } 	
+	
+
+	int total_mark = 0;
+	
+	
+	for (int i=0; i<= NUMBER_QUESTION; i++){
+		if(exam[i].q_id == sol[i].s_id && exam[i].ans == sol[i].ans){
+			total_mark += total_mark;
+		} else{
+			total_mark = total_mark;
+		}
+	}
+	
+	
+	fputs(STUDENT_ID, fPtr);
+   	fputs(student_id, fPtr);
+	
+	fputs(GRADE, fPtr);
+   	fputs(total_mark, fPtr);
+	
+	fclose(fPtr);
+	
+	printf("You result is: %d", total_mark);
+}
+
+
+
+
+
